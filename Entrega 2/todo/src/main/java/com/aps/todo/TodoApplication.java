1992ad95458da93073b2e502189a17400d9427d9
RodrigoMesel <rmlo@cin.ufpe.br>
2023-07-22 10:00:51 -0300

    google login

--- Entrega 2/todo/src/main/java/com/aps/todo/TodoApplication.java
+++ Entrega 2/todo/src/main/java/com/aps/todo/TodoApplication.java
@@ -2,6 +2,7 @@ package com.aps.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class TodoApplication {
--- Entrega 2/todo/src/main/java/com/aps/todo/repositories/UserRepository.java
+++ Entrega 2/todo/src/main/java/com/aps/todo/repositories/UserRepository.java
@@ -2,11 +2,10 @@ package com.aps.todo.repositories;
 
 import com.aps.todo.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
-
-    // Adicione métodos personalizados de consulta, se necessário
 
 }
